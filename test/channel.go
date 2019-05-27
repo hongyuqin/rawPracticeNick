ac90210a0ba37282b4edae1784b39e2cf9597adc
hongyuqin <yuqin_hong@kingdee.com>
2019-05-27 08:51:52 +0800

    测试channel是否会阻塞等待

--- test/channel.go
+++ test/channel.go
@@ -1,12 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func sum(s []int, c chan int) {
 	sum := 0
 	for _, v := range s {
 		sum += v
 	}
+	time.Sleep(time.Duration(10) * time.Second)
 	c <- sum // 把 sum 发送到通道 c
 }
 
@@ -18,5 +22,6 @@ func main() {
 	go sum(s[len(s)/2:], c)
 	x, y := <-c, <-c // 从通道 c 中接收
 
+	//阻塞的
 	fmt.Println(x, y, x+y)
 }
