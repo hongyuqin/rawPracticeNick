468d5d43543a46524bff514bffb7b6b8483f7b75
nick.hong <nick.hong@klook.com>
2019-06-11 15:07:14 +0800

    test

--- work_test_main.go
+++ work_test_main.go
@@ -29,11 +29,66 @@ func testListFor() {
 	}
 
 }
+
+//4.recover捕获panic
+func panicF() {
+	fmt.Println("a")
+	panic(55)
+	fmt.Println("b")
+	fmt.Println("f")
+}
+func recoverTest() {
+	defer func() { // 必须要先声明defer，否则不能捕获到panic异常
+		fmt.Println("c")
+		if err := recover(); err != nil {
+			fmt.Println(err) // 这里的err其实就是panic传入的内容，55
+		}
+		fmt.Println("d")
+	}()
+	panicF()
+}
+
+//5.枚举类型
+type State int
+
+const (
+	Running State = iota
+	Stopped
+	Rebooting
+	Terminated
+)
+
+func (s State) String() string {
+	switch s {
+	case Running:
+		return "Running"
+	case Stopped:
+		return "Stopped"
+	case Rebooting:
+		return "Rebooting"
+	case Terminated:
+		return "Terminated"
+	default:
+		return "Unknown"
+	}
+}
+
+type T struct {
+	Name  string
+	Port  int
+	State State
+}
+
 func main() {
+	//5.
+	t := T{Name: "example", Port: 6666}
+	fmt.Printf("t %+v\n", t)
+
+	recoverTest()
 	//1.str->int
 	//a,_ := strconv.Atoi("")
 	//fmt.Println(a)
 	//testPrefix()
 	//testMapFor()
-	testListFor()
+	//testListFor()
 }
