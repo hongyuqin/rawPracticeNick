f2f4a2c54ae12b157b9eb0b928ee21aa9e1a7423
nick.hong <nick.hong@klook.com>
2019-07-02 18:11:51 +0800

    bug fix

--- anothertest/defer/deferReturn.go
+++ anothertest/defer/deferReturn.go
@@ -2,10 +2,21 @@ package main
 
 import "fmt"
 
-func testReturn() {
-	return
-	defer fmt.Println("hello world")
+func testFallThrough() {
+	s := "abcd"
+	switch s[0] {
+	case 'a':
+		fmt.Println("The integer was <= 4")
+		fallthrough
+	case 'b':
+		fmt.Println("The integer was <= 5")
+		fallthrough
+	case 'c':
+		fmt.Println("The integer was <= 6")
+	default:
+		fmt.Println("default case")
+	}
 }
 func main() {
-	testReturn()
+	testFallThrough()
 }
